73a53493aa2a44d79181a90e32330812d65194f2
Danilo Pascoalim <92922956+dfpascoalim@users.noreply.github.com>
2022-01-30 11:32:52 -0300

    Update sorteador_mega_sena.py
    Adicionando mensagens de boas vindas e comentários

--- sorteador_mega_sena.py
+++ sorteador_mega_sena.py
@@ -1,16 +1,14 @@
-'''
-FONTE: NECESSIDADE
-NÚMERO: N/A
-STATUS: NÃO
-CONFERIDO COM GABARITO: N/A
-DATA: 27/01/2022
-'''
 '''Faça um programa que sorteie 6 dezenas para o concurso da Mega Sena. As dezenas não devem se repetir!'''
 
 from random import randint as rd
 from os import system as st
 from time import sleep
-st('cls')
+st('cls') #limpa o terminal no VSCODE
+
+print("Olá, seja bem vindo(a)!")
+sleep(1)
+print("Vou sortear o seu jogo!")
+sleep(1)
 
 qt_dezenas = 6
 num_sorteados = list()
@@ -28,4 +26,7 @@ num_sorteados.sort()
 print('Os números sorteados para o concurso da Mega Sena foram:')
 for c in range(0,len(num_sorteados)):
     print(f'{c+1}ª dezena: {num_sorteados[c]}')
-    sleep(0.1)
+    sleep(0.2)
+
+    
+print('Boa sorte!')
